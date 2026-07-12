5fa1ddd82845ab1b76f68da14f27c39228bbd999
Kenesa <kenesatesfaye638@gamil.com>
2026-07-12 09:20:09 -0700

    fixed the incident page

--- app/incidents/[id]/page.tsx
+++ app/incidents/[id]/page.tsx
@@ -1,12 +1,17 @@
-import {incidents , Incident}  from "../../api/incidents/data";
+import { Incident}  from "../../api/incidents/data";
 import {notFound} from "next/navigation";
+import {supabase} from "../../lib/supabase"
 
 export default async function IncidentPage({ params }: { params:Promise<{ id: string }>} ) {
 
     const { id } = await params;
-    const incident =  incidents.find((i) => i.id === id);
+  const { data, error } = await supabase
+    .from('incidents')
+    .select('*')
+    .eq('id', id)
+    .single();;
 
-    if (!incident) {
+    if (!data) {
         notFound();
         return
     }
@@ -14,9 +19,9 @@ export default async function IncidentPage({ params }: { params:Promise<{ id: st
 
     return (
         <div>
-            <h1>{incident.id}</h1>
-            <h1>{incident.title}</h1>
-            <p>{incident.severity}</p>
+            <h1>{data.id}</h1>
+            <h1>{data.title}</h1>
+            <p>{data.severity}</p>
             
         </div>
     );
