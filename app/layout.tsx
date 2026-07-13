71505e2c868e415252e60b3c85ef7522968d05ba
Kenesa <kenesatesfaye638@gamil.com>
2026-07-13 15:04:14 -0700

    Day 15- Designing Landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-screen bg-slate-900 ">
         <Nav /> 
         
         {children}
